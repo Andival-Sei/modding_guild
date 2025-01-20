5533f595f0f02804cd7493df0be3b7f1b5ce4da4
Kirill Barkov <freedomdragon777@gmail.com>
2025-01-20 12:58:31 +0400

    Добавлена адаптивность для header и начат бургер.

---
+++ scripts/script.js
@@ -0,0 +1,7 @@
+const burger = document.querySelector(".header__burger");
+const headerMenu = document.querySelector('.header__menu')
+
+burger.addEventListener('click', () => {
+    burger.classList.toggle("active");
+    headerMenu.classList.toggle("active")
+})
